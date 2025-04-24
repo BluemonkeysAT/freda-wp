51ca7a2b68268313b92d4dfd812129318ba76712
Gregor Wallner <greg.wallner@gmail.com>
2025-04-24 12:39:48 +0200

    Header Tiktok Merge

--- header.php
+++ header.php
@@ -43,9 +43,9 @@
                     <button type="submit" class="search-button"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/search-icon.svg" alt="search icon"></button>
                 </form>
                 <div class="social-icons">
-                    <a href="https://www.instagram.com/freda_magazin/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/instagram-icon.svg" alt="Instagram"></a>
-                    <a href="https://www.facebook.com/fredamagazine" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/facebook-icon.svg" alt="Facebook"></a>
-                    <a href="https://www.tiktok.com/@freda_magazin" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/tiktok-icon.svg" alt="Tiktok"></a>
+                    <a href="<?php echo get_field('facebook', 'options'); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/instagram-icon.svg" alt="Instagram"></a>
+                    <a href="<?php echo get_field('instagram', 'options'); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/facebook-icon.svg" alt="Facebook"></a>
+                    <a href="<?php echo get_field('tiktok', 'options'); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/tiktok-icon.svg" alt="Tiktok"></a>
                 </div>
             </div>
         </div>
